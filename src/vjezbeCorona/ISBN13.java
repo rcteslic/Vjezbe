43180fe7ace43b876b8b81e4dff6df54a5548934
vojo <vojislav.kovanusic@infobip.com>
2021-03-25 19:37:42 +0100

    dodao ternarni operator i koji komentar

--- src/vjezbeCorona/ISBN13.java
+++ src/vjezbeCorona/ISBN13.java
@@ -3,9 +3,11 @@ package vjezbeCorona;
 public class ISBN13 {
     // 10 - (d 1 + 3d 2 + d 3 + 3d 4 + d 5 + 3d 6 + d 7 + 3d 8 + d 9 + 3d 10 + d 11 + 3d 12 )%10
     public static void main(String[] args) {
-          String unos = "978013213080";
-        // String unos = "978013213079";  nula na kraju mora biti
+        // Ovdje treba sa Skenerom uhvatiti od korisnika unos, aj sam  uprostio sa ove tri varijante iz knjige
+         String unos = "978013213080";
+        //  String unos = "978013213079";   nula na kraju mora biti
         // String unos = "97801320"; invalidan unos
+        // odma na startu odradio provjeru , ako je invalidan unos domah prekidam dalji tok programa
         if (unos.length() != 12) {
             System.out.println("Invalidan input");
             System.exit(400);
@@ -18,10 +20,11 @@ public class ISBN13 {
                 d += Character.getNumericValue(unos.charAt(i)) * 3;
             }
         }
+
         int zadnjaCifra = 10 - d % 10;
-        if (zadnjaCifra == 10) {
-            zadnjaCifra = 0;
-        }
+
+        // ternarni operator idealan za ovo
+        zadnjaCifra = (zadnjaCifra == 10) ? 0 : zadnjaCifra;
 
         System.out.println(unos + zadnjaCifra);
     }
